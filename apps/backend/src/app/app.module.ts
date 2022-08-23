6c9040b32ac7c8115c22a1362b564e5894818c50
jowell <87069318+JoelDigbeu@users.noreply.github.com>
2022-08-23 15:59:41 +0000

    add graphModule configuration

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { BackendGroupModule } from '@nx-fullstack/backend/group'
 import { BackendMemberModule } from '@nx-fullstack/backend/member'
+import { BackendCoreModule } from '@nx-fullstack/backend/core'
 
 @Module({
-  imports: [BackendGroupModule, BackendMemberModule],
+  imports: [BackendGroupModule, BackendMemberModule, BackendCoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
