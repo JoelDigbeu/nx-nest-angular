65419d66471b9dff64d5eb7c8fe8a5ac4629581d
jowell <87069318+JoelDigbeu@users.noreply.github.com>
2022-08-23 17:22:20 +0000

    reset the project

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { join } from 'path'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo'
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
